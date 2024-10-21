0060486819086f811f8daa6c45d7c81db1fde5ac
KIRSCH <anas83kirsch@gmail.com>
2024-10-21 14:17:37 +0200

    ajout

---
+++ jeu.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define PIERRE 1
+#define FEUILLE 2
+#define CISEAUX 3
+#define MAX 255
+
+int main(){
+
+    
+
+
+
+
+    return 0;
+}
